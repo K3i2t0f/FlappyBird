4f531e97f3ce5ec1d798d77c2793690c1bcd80d5
K3i2t0f <borbarakika@gmail.com>
2026-01-21 11:54:26 +0100

    menü létrehozás

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,88 +30,87 @@ namespace FlappyBird
 
         Random rnd = new Random();
 
+        bool gameLoaded = false;
         bool gameStarted = false;
         bool isGameOver = false;
-        bool waitingForRestart = false;
 
         int score = 0;
+        const double PIPE_FULL_HEIGHT = 354;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            topPipes = new Rectangle[]
-            {
-                PipeTop1, PipeTop2, PipeTop3
-            };
-
-            bottomPipes = new Rectangle[]
-            {
-                PipeBottom1, PipeBottom2, PipeBottom3
-            };
+            topPipes = new[] { PipeTop1, PipeTop2, PipeTop3 };
+            bottomPipes = new[] { PipeBottom1, PipeBottom2, PipeBottom3 };
 
             gameTimer.Interval = TimeSpan.FromMilliseconds(20);
             gameTimer.Tick += GameLoop;
-
-            ResetGame();
-
-            Loaded += (_, __) => GameCanvas.Focus();
         }
 
-        // TELJES RESET (indításkor és restartnál)
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu.Visibility = Visibility.Collapsed;
+            DifficultyMenu.Visibility = Visibility.Visible;
+        }
+
+        private void EasyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuOverlay.Visibility = Visibility.Collapsed;
+            BackButton.Visibility = Visibility.Visible;
+            StartEasyGame();
+        }
+
+        private void StartEasyGame()
+        {
+            ResetGame();
+
+            gameLoaded = true;
+            gameStarted = false;
+
+            ScoreText.Visibility = Visibility.Visible;
+            Bird.Visibility = Visibility.Visible;
+
+            foreach (var p in topPipes) p.Visibility = Visibility.Visible;
+            foreach (var p in bottomPipes) p.Visibility = Visibility.Visible;
+
+            GameCanvas.Focus();
+        }
+
         private void ResetGame()
         {
             gameTimer.Stop();
-
             gameStarted = false;
             isGameOver = false;
-            waitingForRestart = false;
 
             birdVelocity = 0;
-
             score = 0;
             ScoreText.Text = "0";
 
             Canvas.SetTop(Bird, 217);
-
             GameOverPanel.Visibility = Visibility.Collapsed;
 
             for (int i = 0; i < 3; i++)
             {
                 Canvas.SetLeft(topPipes[i], 800 + i * 250);
                 Canvas.SetLeft(bottomPipes[i], 800 + i * 250);
-
                 pipeScored[i] = false;
                 ResetPipe(i);
             }
         }
 
-        // Játék elindítása
-        private void StartGame()
-        {
-            gameStarted = true;
-            isGameOver = false;
-            birdVelocity = 0;
-            gameTimer.Start();
-        }
-
-        // Fő játék ciklus
         private void GameLoop(object sender, EventArgs e)
         {
-            if (!gameStarted)
-                return;
+            if (!gameStarted || isGameOver) return;
 
-            // Madár gravitáció
             birdVelocity += gravity;
             Canvas.SetTop(Bird, Canvas.GetTop(Bird) + birdVelocity);
 
             for (int i = 0; i < 3; i++)
             {
-                // Csövek mozgatása
                 Canvas.SetLeft(topPipes[i], Canvas.GetLeft(topPipes[i]) - pipeSpeed);
                 Canvas.SetLeft(bottomPipes[i], Canvas.GetLeft(bottomPipes[i]) - pipeSpeed);
 
-                // Cső visszarakása jobbra
                 if (Canvas.GetLeft(topPipes[i]) < -100)
                 {
                     double maxX = 0;
@@ -120,12 +119,10 @@ namespace FlappyBird
 
                     Canvas.SetLeft(topPipes[i], maxX + 250);
                     Canvas.SetLeft(bottomPipes[i], maxX + 250);
-
                     pipeScored[i] = false;
                     ResetPipe(i);
                 }
 
-                // Pontozás
                 if (!pipeScored[i] &&
                     Canvas.GetLeft(topPipes[i]) + topPipes[i].Width <
                     Canvas.GetLeft(Bird))
@@ -135,43 +132,36 @@ namespace FlappyBird
                     pipeScored[i] = true;
                 }
 
-                // Ütközés
                 CheckCollision(topPipes[i]);
                 CheckCollision(bottomPipes[i]);
             }
 
-            // Fent vagy lent kiesés
-            if (Canvas.GetTop(Bird) < 0 ||
-                Canvas.GetTop(Bird) + Bird.Height > 450)
-            {
+            if (Canvas.GetTop(Bird) < 0 || Canvas.GetTop(Bird) + Bird.Height > 450)
                 GameOver();
-            }
         }
 
-        // Csövek magasság + rés
         private void ResetPipe(int index)
         {
             int gap = rnd.Next(130, 180);
-            int topHeight = rnd.Next(80, 200);
+            double gapCenterY = rnd.Next(170, 280);
 
-            topPipes[index].Height = topHeight;
-            bottomPipes[index].Height = 450 - (topHeight + gap);
+            double gapTop = gapCenterY - gap / 2;
+            double gapBottom = gapCenterY + gap / 2;
 
-            Canvas.SetTop(topPipes[index], 0);
-            Canvas.SetTop(bottomPipes[index], topHeight + gap);
+            topPipes[index].Height = PIPE_FULL_HEIGHT;
+            Canvas.SetTop(topPipes[index], gapTop - PIPE_FULL_HEIGHT);
+
+            bottomPipes[index].Height = 450 - gapBottom;
+            Canvas.SetTop(bottomPipes[index], gapBottom);
         }
 
-        // Ütközés ellenőrzés
         private void CheckCollision(Rectangle pipe)
         {
-            if (isGameOver)
-                return;
-
             Rect birdRect = new Rect(
-                Canvas.GetLeft(Bird)+14,
-                Canvas.GetTop(Bird)+14,
-                Bird.Width-28,
-                Bird.Height-28);
+                Canvas.GetLeft(Bird) + 14,
+                Canvas.GetTop(Bird) + 14,
+                Bird.Width - 28,
+                Bird.Height - 28);
 
             Rect pipeRect = new Rect(
                 Canvas.GetLeft(pipe),
@@ -183,43 +173,51 @@ namespace FlappyBird
                 GameOver();
         }
 
-        // Játék vége
         private void GameOver()
         {
-            if (isGameOver)
-                return;
-
             isGameOver = true;
             gameStarted = false;
-            waitingForRestart = true;
-
             gameTimer.Stop();
 
             FinalScoreText.Text = $"Pontszám: {score}";
             GameOverPanel.Visibility = Visibility.Visible;
         }
 
-        // SPACE kezelés
         private void Canvas_KeyDown(object sender, KeyEventArgs e)
         {
-            // Halál után SPACE teljesen tiltva
-            if (waitingForRestart)
-                return;
-
-            if (e.Key != Key.Space)
-                return;
+            if (!gameLoaded || isGameOver) return;
+            if (e.Key != Key.Space) return;
 
             if (!gameStarted)
             {
-                StartGame();
-                birdVelocity = -10;
-                return;
+                gameStarted = true;
+                gameTimer.Start();
             }
 
             birdVelocity = -10;
         }
 
-        // Restart gomb
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            gameTimer.Stop();
+            gameLoaded = false;
+            gameStarted = false;
+            isGameOver = false;
+
+            BackButton.Visibility = Visibility.Collapsed;
+
+            ScoreText.Visibility = Visibility.Collapsed;
+            Bird.Visibility = Visibility.Collapsed;
+            GameOverPanel.Visibility = Visibility.Collapsed;
+
+            foreach (var p in topPipes) p.Visibility = Visibility.Collapsed;
+            foreach (var p in bottomPipes) p.Visibility = Visibility.Collapsed;
+
+            MenuOverlay.Visibility = Visibility.Visible;
+            MainMenu.Visibility = Visibility.Collapsed;
+            DifficultyMenu.Visibility = Visibility.Visible;
+        }
+
         private void RestartButton_Click(object sender, RoutedEventArgs e)
         {
             ResetGame();
